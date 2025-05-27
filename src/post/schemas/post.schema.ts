75c158166a6a8335892101a036273b038e0a55e2
dayul <dyulchoo@naver.com>
2025-05-27 18:31:01 +0900

    ✨[feat] : Post, Tag, User schema 추가

---
+++ src/post/schemas/post.schema.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+
+export type PostDocument = HydratedDocument<Post>;
+
+@Schema()
+export class Post {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  user: mongoose.Schema.Types.ObjectId;
+
+  @Prop()
+  imageUrl: string;
+
+  @Prop()
+  location: string;
+
+  @Prop({ type: mongoose.Schema.Types.Date })
+  date: Date;
+
+  @Prop()
+  memo: string;
+
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tag' }] })
+  tags: mongoose.Schema.Types.ObjectId[];
+}
+
+export const PostSchema = SchemaFactory.createForClass(Post);
